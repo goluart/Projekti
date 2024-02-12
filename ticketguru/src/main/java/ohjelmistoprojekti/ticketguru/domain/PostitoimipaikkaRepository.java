46db12196d3633dee4e43030dc7e382eac7a0d37
JouniAUT <jouni.au.tuomela@gmail.com>
2024-02-12 13:15:30 +0200

    PostioimipaikkaRepository luotu

---
+++ ticketguru/src/main/java/ohjelmistoprojekti/ticketguru/domain/PostitoimipaikkaRepository.java
@@ -0,0 +1,7 @@
+package ohjelmistoprojekti.ticketguru.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostitoimipaikkaRepository extends CrudRepository<Postitoimipaikka, Long> {
+
+}
