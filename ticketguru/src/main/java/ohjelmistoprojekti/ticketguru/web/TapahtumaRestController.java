aba9b7b3d6242deafd523eaf38db1beb07852bd4
JouniAUT <jouni.au.tuomela@gmail.com>
2024-02-18 10:58:33 +0200

    Tapahtuma REST API .findAll() luotu

--- ticketguru/src/main/java/ohjelmistoprojekti/ticketguru/web/TapahtumaRestController.java
+++ ticketguru/src/main/java/ohjelmistoprojekti/ticketguru/web/TapahtumaRestController.java
@@ -1,8 +1,31 @@
 package ohjelmistoprojekti.ticketguru.web;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.List;
 
-@RestController
+import ohjelmistoprojekti.ticketguru.domain.Tapahtuma;
+import ohjelmistoprojekti.ticketguru.domain.TapahtumaRepository;
+
+
+
+@CrossOrigin
+@Controller
 public class TapahtumaRestController {
+
+    @Autowired
+    private TapahtumaRepository tapahtumaRepository;
+    
+    @RequestMapping(value="/tapahtumat", method = RequestMethod.GET)
+    public @ResponseBody List<Tapahtuma> tapahtumatListRest() {
+    	return (List<Tapahtuma>)tapahtumaRepository.findAll();
+    }
+
+    
+    
 	
 }
