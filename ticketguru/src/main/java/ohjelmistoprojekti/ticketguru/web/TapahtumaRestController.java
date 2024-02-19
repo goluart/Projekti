cd8accfff70c8affb7fcdafd9d1ddc9d646fe515
HMVarp <bha605@myy.haaga-helia.fi>
2024-02-19 17:55:06 +0200

    Post lisätty ja testattu, versiotietojen poistoa ym. pieniä muutoksia

--- ticketguru/src/main/java/ohjelmistoprojekti/ticketguru/web/TapahtumaRestController.java
+++ ticketguru/src/main/java/ohjelmistoprojekti/ticketguru/web/TapahtumaRestController.java
@@ -1,17 +1,19 @@
 package ohjelmistoprojekti.ticketguru.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.List;
 
 import ohjelmistoprojekti.ticketguru.domain.Tapahtuma;
 import ohjelmistoprojekti.ticketguru.domain.TapahtumaRepository;
-
-
 
 @CrossOrigin
 @Controller
@@ -24,8 +26,10 @@ public class TapahtumaRestController {
     public @ResponseBody List<Tapahtuma> tapahtumatListRest() {
     	return (List<Tapahtuma>)tapahtumaRepository.findAll();
     }
-
     
-    
-	
+    //@NotNull lisätty virheilmoituksen perusteella
+    @PostMapping("tapahtuma")
+	Tapahtuma newTapahtuma(@RequestBody @NonNull Tapahtuma newTapahtuma) {
+		return tapahtumaRepository.save(newTapahtuma);
+	}
 }
