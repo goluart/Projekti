d1f2681e3734d38dc15aa60a3b357ed29ff06db4
HMVarp <bha605@myy.haaga-helia.fi>
2024-03-10 21:05:35 +0200

    Get metodeille laitettu vastauskoodeja

--- ticketguru/src/main/java/ohjelmistoprojekti/ticketguru/web/TapahtumaRestController.java
+++ ticketguru/src/main/java/ohjelmistoprojekti/ticketguru/web/TapahtumaRestController.java
@@ -25,63 +25,67 @@ import ohjelmistoprojekti.ticketguru.domain.TapahtumaRepository;
 @RestController
 public class TapahtumaRestController {
 
-    @Autowired
-    private TapahtumaRepository tapahtumaRepository;
+	@Autowired
+	private TapahtumaRepository tapahtumaRepository;
 
-    // Haetaan kaikki järjestelmän tapahtumat
-    // Muutettu @RequestMapping @GetMapping muotoon
-    @GetMapping("/tapahtumat")
-    public List<Tapahtuma> tapahtumatListRest() {
-        return tapahtumaRepository.findAll();
-    }
+	// Haetaan kaikki järjestelmän tapahtumat
+	// Muutettu @RequestMapping @GetMapping muotoon
+	// Jos get-metodi palauttaa tyhjän listan, palautetaan 404
+	@GetMapping("/tapahtumat")
+	public List<Tapahtuma> tapahtumatListRest() {
+		if (tapahtumaRepository.findAll().isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Tapahtumia ei löytynyt.");
+		}
+		return tapahtumaRepository.findAll();
+	}
 
-    // Haetaan tapahtuma tunnisteen (tapahtumaId) avulla
-    @GetMapping("/tapahtumat/{id}")
-    public Optional<Tapahtuma> findTapahtumaById(@PathVariable("id") @NonNull Long tapahtumaId) {
-    	
-        return tapahtumaRepository.findById(tapahtumaId);
-    }
+	// Haetaan tapahtuma tunnisteen (tapahtumaId) avulla
+	// Jos get-metodi palauttaa tyhjän vastauksen, palautetaan 404
+	@GetMapping("/tapahtumat/{id}")
+	public Optional<Tapahtuma> findTapahtumaById(@PathVariable("id") @NonNull Long tapahtumaId) {
+		if (tapahtumaRepository.findById(tapahtumaId).isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Tapahtumaa " + tapahtumaId + " ei löytynyt.");
+		}
+		return tapahtumaRepository.findById(tapahtumaId);
+	}
 
-    // @NotNull lisätty virheilmoituksen perusteella
-    @PostMapping("/tapahtumat")
-    @ResponseStatus(value = HttpStatus.CREATED, reason = "Uusi tapahtuma luotu")
-    public Tapahtuma newTapahtuma(@RequestBody @NonNull Tapahtuma newTapahtuma) {        	
-    	return tapahtumaRepository.save(newTapahtuma);
-    }
+	// @NotNull lisätty virheilmoituksen perusteella
+	@PostMapping("/tapahtumat")
+	@ResponseStatus(value = HttpStatus.CREATED, reason = "Uusi tapahtuma luotu")
+	public Tapahtuma newTapahtuma(@RequestBody @NonNull Tapahtuma newTapahtuma) {
+		return tapahtumaRepository.save(newTapahtuma);
+	}
 
-    // Poista tapahtuma, tapahtuma IDllä esim. "localhost:8080/tapahtumat/1"
-    // Palautetaan "204 No Content" status koodi onnistuessa, sopivampi tilakoodi
-    // onnistuneille DELETE-pyynnöille, koska niitä resursseja ei enää ole olemassa.
-    @DeleteMapping("/tapahtumat/{id}")
-    public ResponseEntity<Void> deleteTapahtuma(@PathVariable("id") @NonNull Long tapahtumaId) {
-        Optional<Tapahtuma> tapahtumaOptional = tapahtumaRepository.findById(tapahtumaId);
+	// Poista tapahtuma, tapahtuma IDllä esim. "localhost:8080/tapahtumat/1"
+	// Palautetaan "204 No Content" status koodi onnistuessa, sopivampi tilakoodi
+	// onnistuneille DELETE-pyynnöille, koska niitä resursseja ei enää ole olemassa.
+	@DeleteMapping("/tapahtumat/{id}")
+	public ResponseEntity<Void> deleteTapahtuma(@PathVariable("id") @NonNull Long tapahtumaId) {
+		Optional<Tapahtuma> tapahtumaOptional = tapahtumaRepository.findById(tapahtumaId);
 
-        if (tapahtumaOptional.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Tapahtumaa " + tapahtumaId + " ei löytynyt.");
-        }
+		if (tapahtumaOptional.isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Tapahtumaa " + tapahtumaId + " ei löytynyt.");
+		}
 
-        // Tarkistetaan onko lippuja, jos lippuja löytyy tapahtumaa ei voida poistaa
-        // koska siihen on myyty lippuja. Tällä hetkellä voidaan vaan poistaa jos
-        // lippuja on 0 tai max määrä.
-        Tapahtuma tapahtuma = tapahtumaOptional.get();
-        int jaljellaLippuja = tapahtuma.getMax_lippuja() - tapahtuma.getLiput().size();
+		// Tarkistetaan onko lippuja, jos lippuja löytyy tapahtumaa ei voida poistaa
+		// koska siihen on myyty lippuja. Tällä hetkellä voidaan vaan poistaa jos
+		// lippuja on 0 tai max määrä.
+		Tapahtuma tapahtuma = tapahtumaOptional.get();
+		int jaljellaLippuja = tapahtuma.getMax_lippuja() - tapahtuma.getLiput().size();
 
-        if (jaljellaLippuja != 0 && jaljellaLippuja != tapahtuma.getMax_lippuja()) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
-                    "Tapahtumaa " + tapahtumaId + " ei voi poistaa, koska tapahtumaan on myyty lippuja.");
-        }
+		if (jaljellaLippuja != 0 && jaljellaLippuja != tapahtuma.getMax_lippuja()) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+					"Tapahtumaa " + tapahtumaId + " ei voi poistaa, koska tapahtumaan on myyty lippuja.");
+		}
+		tapahtumaRepository.delete(tapahtuma);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+	}
 
-        tapahtumaRepository.delete(tapahtuma);
-
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-    }
-
-    // Etsi yksi tapahtuma muokkaamista varten
-    // Muokkaa tunniteella yksilöityä tapahtumaa ja tallenna tehdyt muutokset
-    @PutMapping("tapahtumat/{id}")
-    public Tapahtuma editTapahtuma(@RequestBody Tapahtuma editedTapahtuma, @PathVariable Long id) {
-        editedTapahtuma.setTapahtumaId(id);
-        return tapahtumaRepository.save(editedTapahtuma);
-
-    }
+	// Etsi yksi tapahtuma muokkaamista varten
+	// Muokkaa tunniteella yksilöityä tapahtumaa ja tallenna tehdyt muutokset
+	@PutMapping("tapahtumat/{id}")
+	public Tapahtuma editTapahtuma(@RequestBody Tapahtuma editedTapahtuma, @PathVariable Long id) {
+		editedTapahtuma.setTapahtumaId(id);
+		return tapahtumaRepository.save(editedTapahtuma);
+	}
 }
