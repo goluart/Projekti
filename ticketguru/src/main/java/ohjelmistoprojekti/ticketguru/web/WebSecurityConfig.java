7b2e2144f8382d859ac48c3a6ae16ec72153d439
goluart <bgs310@myy.haaga-helia.fi>
2024-03-18 04:17:46 +0200

    lisätty httpbasic

--- ticketguru/src/main/java/ohjelmistoprojekti/ticketguru/web/WebSecurityConfig.java
+++ ticketguru/src/main/java/ohjelmistoprojekti/ticketguru/web/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package ohjelmistoprojekti.ticketguru.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -24,7 +25,9 @@ public class WebSecurityConfig {
                         .anyRequest().authenticated())
                 .formLogin((form) -> form
                         .permitAll())
-                .logout((logout) -> logout.permitAll());
+                .logout((logout) -> logout.permitAll())
+                // https://docs.spring.io/spring-security/reference/servlet/authentication/passwords/basic.html
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
