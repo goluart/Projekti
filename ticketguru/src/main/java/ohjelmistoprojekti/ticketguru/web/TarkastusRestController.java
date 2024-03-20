1a816a79b848d21ddb38dffe7bff1298244192b5
JouniAUT <jouni.au.tuomela@gmail.com>
2024-03-20 13:38:14 +0200

    Tarkastus-luokka, TarkastusDTO, TarkastusService ja TarkastusRepository

---
+++ ticketguru/src/main/java/ohjelmistoprojekti/ticketguru/web/TarkastusRestController.java
@@ -0,0 +1,22 @@
+package ohjelmistoprojekti.ticketguru.web;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ohjelmistoprojekti.ticketguru.dto.TarkastusDTO;
+import ohjelmistoprojekti.ticketguru.service.TarkastusService;
+
+@RestController
+public class TarkastusRestController {
+
+    @Autowired
+    private TarkastusService tarkastusService;
+
+    @GetMapping("/tarkastukset")
+    public List<TarkastusDTO> haeKaikkiTarkastukset() {
+        return tarkastusService.haeKaikkiTarkastukset();
+    }
+}
