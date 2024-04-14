5d47a2402130adf75bb17bc48bed2cc2b5488d13
JouniAUT <jouni.au.tuomela@gmail.com>
2024-04-14 15:01:50 +0300

    Lippujen lisäystä

--- ticketguru/src/main/java/ohjelmistoprojekti/ticketguru/dto/LuoMyyntitapahtumaDTO.java
+++ ticketguru/src/main/java/ohjelmistoprojekti/ticketguru/dto/LuoMyyntitapahtumaDTO.java
@@ -2,9 +2,8 @@ package ohjelmistoprojekti.ticketguru.dto;
 
 import java.util.List;
 
-
 public class LuoMyyntitapahtumaDTO {
-    
+
     private Long tapahtumaId;
     private List<LippuTyyppiMaaraDTO> lippuTyyppiMaarat;
 
@@ -19,37 +18,43 @@ public class LuoMyyntitapahtumaDTO {
     public Long getTapahtumaId() {
         return tapahtumaId;
     }
+
     public void setTapahtumaId(Long tapahtumaId) {
         this.tapahtumaId = tapahtumaId;
     }
+
     public List<LippuTyyppiMaaraDTO> getLippuTyyppiMaarat() {
         return lippuTyyppiMaarat;
     }
+
     public void setLippuTyyppiMaarat(List<LippuTyyppiMaaraDTO> lippuTyyppiMaarat) {
         this.lippuTyyppiMaarat = lippuTyyppiMaarat;
-    }   
+    }
 
     public static class LippuTyyppiMaaraDTO {
 
-
         private Long lipputyyppiId;
         private int lippuMaara;
-       
+
         public LippuTyyppiMaaraDTO(Long lipputyyppiId, int lippuMaara) {
             this.lipputyyppiId = lipputyyppiId;
             this.lippuMaara = lippuMaara;
         }
+
         public Long getLipputyyppiId() {
             return lipputyyppiId;
         }
+
         public void setLipputyyppiId(Long lipputyyppiId) {
             this.lipputyyppiId = lipputyyppiId;
         }
+
         public int getLippuMaara() {
             return lippuMaara;
         }
+
         public void setLippuMaara(int lippuMaara) {
             this.lippuMaara = lippuMaara;
         }
-}
+    }
 }
