eb8324b881a4886e36202c672162906cd9c10e34
JouniAUT <jouni.au.tuomela@gmail.com>
2024-04-28 17:33:34 +0300

    Tapahtumapaikka restcontroller aloitettu

---
+++ ticketguru/src/main/java/ohjelmistoprojekti/ticketguru/web/TapahtumapaikkaRestController.java
@@ -0,0 +1,24 @@
+package ohjelmistoprojekti.ticketguru.web;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import ohjelmistoprojekti.ticketguru.domain.TapahtumapaikkaRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+public class TapahtumapaikkaRestController {
+
+    @Autowired
+    private TapahtumapaikkaRepository tapahtumapaikkaRepository;
+
+@GetMapping("/tapahtumapaikka")
+
+
+}
