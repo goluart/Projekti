95989bd96f19052276d97163ebe0d4c306f8ccb2
MiaKariza <mia.huovinen@gmail.com>
2024-05-13 02:09:23 +0300

    "Muutos yhteyshenkilöservisessä"

--- ticketguru/src/main/java/ohjelmistoprojekti/ticketguru/service/YhteyshenkiloService.java
+++ ticketguru/src/main/java/ohjelmistoprojekti/ticketguru/service/YhteyshenkiloService.java
@@ -1,6 +1,5 @@
 package ohjelmistoprojekti.ticketguru.service;
 
-import org.apache.el.stream.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
